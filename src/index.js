a0cfabb8e9a930a00f8439305d70343d61334b67
Zookerman3 <szook7@gmail.com>
2023-02-07 15:57:55 -0800

    created dino hangman game
    NicksFed <nick.guzy76@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,44 +1,90 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-
 // Business Logic
 
-function getWeather(city) {
-  let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
 
-  request.addEventListener("loadend", function() {
-    const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      printElements(response, city);
-    } else {
-      printError(this, response, city);
-    }
+
+
+function getDinoText() {
+  let promise = new Promise(function (resolve, reject) {
+    let request = new XMLHttpRequest();
+    const url = "https://dinoipsum.com/api/?format=json&paragraphs=1&words=1";
+    request.addEventListener("loadend", function () {
+      const response = JSON.parse(this.responseText);
+      if (this.status === 200) {
+        resolve(response);
+      } else {
+        reject(this);
+      }
+    });
+    request.open("GET", url, true);
+    request.send();
   });
 
-  request.open("GET", url, true);
-  request.send();
+  promise.then(function (response) {
+    getApiResponse(response);
+    callCompareUserInput(response);
+  }, function (errorMessage) {
+    printError(errorMessage);
+  });
 }
+
 
 // UI Logic
 
-function printElements(apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `The humidity in ${city} is ${apiResponse.main.humidity}%. 
-  The temperature in Kelvins is ${apiResponse.main.temp} degrees.`;
+function getApiResponse(apiResponse) {
+  let ul = document.getElementById("show-response");
+
+  let dinoArray = apiResponse[0][0].toString().split('');
+
+  let i = 0;
+  console.log(dinoArray);
+  for (let x = 0; x < dinoArray.length; x++) {
+    let pEl = document.createElement('p');
+    let pEl2 = document.createElement('p');
+    pEl.innerText = '_';
+    pEl2.innerText = ' ';
+    pEl.setAttribute("id", `${i}`);
+    console.log(i);
+    ul.append(pEl);
+    ul.append(pEl2);
+    i++;
+  }
 }
 
-function printError(request, apiResponse, city) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+function callCompareUserInput(apiResponse) {
+  document.querySelector('#letter-button').addEventListener("click", function (event) {
+    event.preventDefault();
+    
+    compareUserInput(apiResponse);
+  });
+}
+
+function compareUserInput(apiResponse) {
+  let compareLetter = document.querySelector("#letter").value;
+  let wordArray = apiResponse[0][0].toString().split('');
+  let i = 0;
+  wordArray.forEach(element => {
+    if (element.toUpperCase() === compareLetter.toUpperCase()){
+      console.log("hello");
+      document.getElementById(`${i}`).innerText = element;
+    }
+    i++;
+  });
+}
+
+
+
+function printError(apiResponse) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the dino data for  ${apiResponse}`;
 }
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  const city = document.querySelector('#location').value;
-  document.querySelector('#location').value = null;
-  getWeather(city);
+  getDinoText();
 }
 
-window.addEventListener("load", function() {
-  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+window.addEventListener("load", function () {
+  document.querySelector('#get-dinoText').addEventListener("click", handleFormSubmission);
 });
